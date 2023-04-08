f9dff371560fb41de5408a58e3c218d2f31f5852
王天赐 <15565946702@163.com>
2023-04-08 19:30:10 +0800

    :art: 文件上传下载

---
+++ file-upload-download/src/main/java/cn/knightzz/FileUploadDownloadApplication.java
@@ -0,0 +1,16 @@
+package cn.knightzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王天赐
+ */
+@SpringBootApplication
+public class FileUploadDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadDownloadApplication.class, args);
+    }
+
+}
